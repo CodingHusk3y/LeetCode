2845d4000de91149ac713abbd205921dacc4d37f
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2024-09-26 21:11:43 -0400

    LC-shifting-letters [Runtime: N/A; Memory: N/A]

--- prefix-sum/shifting-letters.py
+++ prefix-sum/shifting-letters.py
@@ -4,7 +4,7 @@ class Solution:
         new_str = ""
 
         for i in range(len(s)):
-            sum_count  = sum(shifts[ i : : ] )
+            sum_count  = sum(shifts)
             char = chr((ord(s[i]) - ord('a') + sum_count) % 26 + ord('a'))
             new_str = new_str + char
 	    
