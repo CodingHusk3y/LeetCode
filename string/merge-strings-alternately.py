1ccd3b805049f9220ab587ffa4bd23410d89b6bb
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2024-12-04 00:35:14 -0500

    LC-merge-strings-alternately [Runtime: 32 ms; Memory: 17.3 MB]

--- string/merge-strings-alternately.py
+++ string/merge-strings-alternately.py
@@ -1,16 +1,16 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        ans = ''
-        i=0
-        j=0
-        m = len(word1)
-        n = len(word2)
-        while i < m or j < n:
-            if i < m:
-                ans += word1[i]
-                i += 1
-            if j < n:
-                ans += word2[j]
-                j += 1
-        return ans
+        result = ""
+        ptr1, ptr2 = 0, 0
+        n = len(word1)
+        m = len(word2)
 
+        while ptr1 < n or ptr2 < m:
+            if ptr1 < n:
+                result += word1[ptr1]
+                ptr1 += 1
+            if ptr2 < m:
+                result +=  word2[ptr2]
+                ptr2 += 1
+
+        return result        
