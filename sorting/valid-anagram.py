a5ee6b6bcf49a81a1044de9d58ef73dd1b356d2c
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2024-12-27 22:32:42 -0500

    LC-valid-anagram [Runtime: 15 ms; Memory: 18.6 MB]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,21 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        if len(s) != len(t): return False
+
+        list_s = list(s)
+        list_t = list(t)
+
+        list_s.sort()
+        list_t.sort()
+
+        return list_s == list_t
         
-        map_s = {}
-        map_t = {}
-
-        for i in s:
-            if i in map_s:
-                map_s[i] += 1
-            else:
-                map_s[i] = 1
-
-        for j in t:
-            if j in map_t:
-                map_t[j] += 1
-            else:
-                map_t[j] = 1
-
-        return map_s == map_t
