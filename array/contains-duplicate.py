db0ed9a3656a3340be98574561c94066389dffb0
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2024-12-27 22:24:17 -0500

    LC-contains-duplicate [Runtime: 19 ms; Memory: 31.4 MB]

--- array/contains-duplicate.py
+++ array/contains-duplicate.py
@@ -1,11 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        dup_map = {}
-
-        for num in nums:
-            if num in dup_map:
-                return True
-            else:
-                dup_map[num] = 1
-
-        return False
+        return not (len(set(nums)) == len(nums)) 
