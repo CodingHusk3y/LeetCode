862b8fcc3f88abda86dca6b0bfdf8cd682fe01ae
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-01-01 23:26:53 -0500

    LC-two-sum [Runtime: 0 ms; Memory: 18.8 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,18 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sorted_nums = sorted(nums)
-        ptr1 = 0
-        ptr2 = len(nums) - 1
+        num_to_index = {}
 
-        while ptr1 < ptr2:
-            if sorted_nums[ptr1] + sorted_nums[ptr2] < target:
-                ptr1 += 1
-            elif sorted_nums[ptr1] + sorted_nums[ptr2] > target:
-                ptr2 -= 1
-            else:
-                # Find the indices of the elements in the original nums list
-                index1 = nums.index(sorted_nums[ptr1])
-                index2 = nums.index(sorted_nums[ptr2])
-                if index1 == index2:
-                    index2 = nums.index(sorted_nums[ptr2], index1 + 1)
-                return [index1, index2]
+        for i, num in enumerate(nums):
+            compliment = target - num
+
+            if compliment in num_to_index:
+                return [num_to_index[compliment], i]
+
+            num_to_index[num] = i
+    
