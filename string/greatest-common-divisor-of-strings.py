1a91d8e997edc15a02a99110b079e3a996560823
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-01-23 21:40:03 -0500

    LC-greatest-common-divisor-of-strings [Runtime: 0 ms; Memory: 17.8 MB]

--- string/greatest-common-divisor-of-strings.py
+++ string/greatest-common-divisor-of-strings.py
@@ -1,14 +1,14 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if len(str1) < len(str2):
-            str1, str2 = str2, str1
-        
-        L, S = len(str1), len(str2)
-        
-        for i in range(S, 0, -1):
-            if (L/i) == int(L/i) and (S/i) == int(S/i):
-                c = str2[:i]
-                if c*int(L/i) == str1 and c*int(S/i) == str2: 
-                    return c
-                
-        return ""
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            
+            return a
+
+        longest_div = gcd(len(str1), len(str2))
+
+        return str1[:longest_div]
