90acfcc5d598f95186ce8171d5dba13b3407969e
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-02-05 15:23:29 -0500

    LC-reverse-words-in-a-string [Runtime: 0 ms; Memory: 17.6 MB]

--- two-pointers/reverse-words-in-a-string.py
+++ two-pointers/reverse-words-in-a-string.py
@@ -1,29 +1,12 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        ans = []
+        words = s.split()
         ptr1 = 0
-        ptr2 = 0
-        n = len(s)
+        ptr2 = len(words) - 1
 
-        while ptr2 < n:
-            # Move ptr1 to the first non-space character
-            while ptr1 < n and s[ptr1] == " ":
-                ptr1 += 1
-            
-            # If ptr1 reaches the end, break
-            if ptr1 >= n:
-                break
+        while ptr1 < ptr2:
+            words[ptr1], words[ptr2] = words[ptr2], words[ptr1]
+            ptr1 += 1
+            ptr2 -= 1
 
-            ptr2 = ptr1  # Start ptr2 at the first character of the word
-            
-            # Move ptr2 to the end of the current word
-            while ptr2 < n and s[ptr2] != " ":
-                ptr2 += 1
-
-            # Extract the word and add to the list
-            ans.append(s[ptr1:ptr2])
-
-            # Move ptr1 to the next word
-            ptr1 = ptr2
-
-        return " ".join(ans[::-1])
+        return " ".join(words)
