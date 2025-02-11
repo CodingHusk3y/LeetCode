f3ed129387982ec71d9d8259c2defe92939142ed
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-02-11 15:14:22 -0500

    LC-increasing-triplet-subsequence [Runtime: N/A; Memory: N/A]

--- greedy/increasing-triplet-subsequence.py
+++ greedy/increasing-triplet-subsequence.py
@@ -1,11 +1,14 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        count = 0
-        ptr1 = 0
+        smallest = float('inf')
+        second_smallest = float('inf')
 
-        for ptr2 in range(len(nums)):
-            if nums[ptr2] > nums[ptr1]:
-                ptr1 = ptr2
-                count += 1
-            
-        return count >= 2
+        for num in nums:
+            if num < smallest:
+                smallest = num
+            elif num < second_smallest:
+                second_smallest = num
+            else:
+                return True
+
+        return False
