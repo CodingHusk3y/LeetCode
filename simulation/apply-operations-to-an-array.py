3209702a7bc4092de823dcdeed0ae9219bd2de94
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-02-28 21:51:06 -0500

    LC-apply-operations-to-an-array [Runtime: N/A; Memory: N/A]

---
+++ simulation/apply-operations-to-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for ptr1 in range(len(nums) - 2):
+            ptr2 = ptr1 + 1
+            
+            if nums[ptr1] == nums[ptr2]:
+                nums[ptr1] *= 2
+                nums[ptr2] = 0
+
+        ptr1 = 0
+        for ptr2 in range(len(nums)):
+            if nums[ptr2] != 0:
+                nums[ptr1], nums[ptr2] = nums[ptr2], nums[ptr1]
+                ptr1 += 1
+
+        return nums
