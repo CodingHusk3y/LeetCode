48b9d9878c47b9e43bd9e1fe5d2f9270a50dd33a
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-05-02 13:55:45 -0400

    LC-push-dominoes [Runtime: 270 ms; Memory: 22.6 MB]

--- two-pointers/push-dominoes.py
+++ two-pointers/push-dominoes.py
@@ -2,27 +2,27 @@ class Solution:
     def pushDominoes(self, dominoes: str) -> str:
         n = len(dominoes)
         forces = [0] * n
-
+        # Left to right
         force = 0
         for i in range(n):
             if dominoes[i] == "R":
-                force += n
+                force = n
             elif dominoes[i] == "L":
                 force = 0
             else:
                 force = max(force - 1, 0) 
             forces[i] += force
-
+        # Right to left
         force = 0
-        for i in range(n, -1, -1):
+        for i in range(n - 1, -1, -1):
             if dominoes[i] == "L":
-                force += n
+                force = n
             elif dominoes[i] == "R":
                 force = 0
             else:
                 force = max(force - 1, 0) 
             forces[i] -= force
-
+        # Final result
         ans = []
         for push in forces:
             if push > 0:
