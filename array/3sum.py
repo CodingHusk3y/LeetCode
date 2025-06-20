7267754448ab1b6dcf70709207b4aed219345f6b
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-06-19 21:53:56 -0400

    LC-3sum [Runtime: 1140 ms; Memory: 20.6 MB]

--- array/3sum.py
+++ array/3sum.py
@@ -2,30 +2,32 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         ans = []
-
-        for i in range(len(nums)):
+        
+        for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+                
+            ptr1 = i + 1
+            ptr2 = len(nums) - 1
 
-            j = i + 1
-            k = len(nums) - 1
-
-            while j < k:
-                total = nums[i] + nums[j] + nums[k]
-                if total > 0:
-                    k -= 1
-                elif total < 0:
-                    j += 1
+            while ptr1 < ptr2:
+                total = nums[i] + nums[ptr1] + nums[ptr2]
+                
+                if total < 0:
+                    ptr1 += 1
+                elif total > 0:
+                    ptr2 -= 1
                 else:
-                    ans.append([nums[i], nums[j], nums[k]])
+                    ans.append([nums[i], nums[ptr1], nums[ptr2]])
 
-                    while j < k and nums[j] == nums[j + 1]:
-                        j += 1
-                    while j < k and nums[k] == nums[k - 1]:
-                        k -= 1
+                    while ptr1 < ptr2 and nums[ptr1] == nums[ptr1 + 1]:
+                        ptr1 += 1
+                    while ptr1 < ptr2 and nums[ptr2] == nums[ptr2 - 1]:
+                        ptr2 -= 1
 
-                    j += 1
-                    k -= 1
+                    ptr1 += 1
+                    ptr2 -= 1
 
-        return ans 
+        return ans
+                    
             
