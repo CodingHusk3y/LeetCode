05dcfc4b34b1a493b9e5dfcdc7fb10634d8032b8
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-06-27 23:12:34 -0400

    LC-valid-parentheses [Runtime: 0 ms; Memory: 18 MB]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -8,7 +8,7 @@ class Solution:
             if string in ["(", "{", "["]:
                 stk.append(string)
             else:
-                if not match(stk[-1], string) or not stk:
+                if not stk or not match(stk[-1], string):
                     return False
                 stk.pop()
 
