755e9eaf9a5d004932f5f28bfa437c9f250f112a
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-06-30 21:22:59 -0400

    LC-evaluate-reverse-polish-notation [Runtime: 4 ms; Memory: 19.2 MB]

--- math/evaluate-reverse-polish-notation.py
+++ math/evaluate-reverse-polish-notation.py
@@ -1,21 +1,26 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        operation = ['+', '-', '*', '/']
         stk = []
+        ops = "+-*/"
+
         for token in tokens:
-            if token in operation:
+            if token in ops and stk:
                 num2 = stk.pop()
                 num1 = stk.pop()
-                if token == '+':
-                    stk.append(num1 + num2)
-                elif token == '*':
-                    stk.append(num1 * num2)
-                elif token == '-':
-                    stk.append(num1 - num2)
+                res = 0
+
+                if token == "+":
+                    res = num1 + num2
+                elif token == "-":
+                    res = num1 - num2
+                elif token == "*":
+                    res = num1 * num2
                 else:
-                    stk.append(int(num1 / num2))
-            
+                    res = int(num1 / num2)
+
+                stk.append(res)
+
             else:
                 stk.append(int(token))
 
-        return stk[0]
+        return stk[-1]
