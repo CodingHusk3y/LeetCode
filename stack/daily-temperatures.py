8274e2f749f53cbd7dbea1d408e48922054710f9
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2025-07-13 01:35:04 -0400

    LC-daily-temperatures [Runtime: 91 ms; Memory: 27.5 MB]

--- stack/daily-temperatures.py
+++ stack/daily-temperatures.py
@@ -1,16 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
-        ans = [0] * n  # Initialize a list of zeros to store results
-        stack = []  # Use a stack to track indices of temperatures
+        ans = [0] * n
+        stk = []
 
-        for i in range(n):
-            while stack and temperatures[i] > temperatures[stack[-1]]:
-                # Update the result for days in the stack
-                idx = stack.pop()
-                ans[idx] = i - idx
-
-            # Push the current day's index onto the stack
-            stack.append(i)
+        for i, temp in enumerate(temperatures):
+            while stk and temperatures[i] > temperatures[stk[-1]]:
+                prev = stk.pop()
+                ans[prev] = i - prev
+            
+            stk.append(i)
 
         return ans
