36e57bc6819bca6199e9887b4d20e083f8d3a557
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2026-03-13 16:50:33 -0400

    LC-top-k-frequent-elements [Runtime: 7 ms; Memory: 22.6 MB]

--- quickselect/top-k-frequent-elements.py
+++ quickselect/top-k-frequent-elements.py
@@ -1,5 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        elements = Counter(nums)
+        # keep track of the occurance of each number
+        # Sort the numbers by it's ocurance
+        # get the k elements 
+        tracking = {}
 
-        return [item[0] for item in elements.most_common(k)]
+        for number in nums:
+            if number in tracking:
+                tracking[number] += 1
+            else:
+                tracking[number] = 1
+
+        tracking = sorted(tracking.items(), key=lambda item: item[1], reverse=True)
+
+        return [num for num, count in tracking[:k]]
+        
