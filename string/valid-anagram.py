297120affda068e49e25c611b6b123df7ac72cb4
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2026-03-13 13:13:47 -0400

    LC-valid-anagram [Runtime: 15 ms; Memory: 20.3 MB]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -1,12 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
-
-        list_s = list(s)
-        list_t = list(t)
-
-        list_s.sort()
-        list_t.sort()
-
-        return list_s == list_t
+        return sorted(s) == sorted(t)
         
